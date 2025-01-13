4a0962ab0cf62e8b1121a2c7680d9e7bc1ed217a
Bắc <you@example.com>
2025-01-13 16:34:12 +0700

    #34

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,24 @@ import './component/todolist/todo.css'
 import { TodoTitle } from './component/todolist/TodoTitle';
 import reactLogo from './assets/react.svg';
 const App =()=>{
+  const a = "Hi guy!"
+  const data ={
+    address :"Hà Nôi",
+    email : "user@gmail.com"
+  }
   return(
     <>
     <div className="todo-container">
       <TodoTitle />
       <TodoNew />
-     <TodoData />
-     
+     <TodoData
+     name={a}
+     data={data}
+      />
     </div>
     <div className="todo-image"> 
       <img src={reactLogo} />
-    </div>
-    </>
+    </div></>
   );
 }
-
 export default App;
--- src/component/todolist/TodoData.jsx
+++ src/component/todolist/TodoData.jsx
@@ -1,10 +1,12 @@
 
 
-const TodoData =()=>{
+const TodoData =({name, data})=>{
     return(
         <div className="todo-data">
         <div> Learning React </div>
         <div> Watching Youtube </div>
+        <div>{name}</div>
+        <div>{data.email}</div>
       </div>
     );
 }
