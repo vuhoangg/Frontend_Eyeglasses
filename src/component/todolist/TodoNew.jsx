58c72cce523b3b1334a1047a53d25dcd01ae573a
Bắc <you@example.com>
2025-01-14 09:32:42 +0700

    #36 Sử dụng event

--- src/component/todolist/TodoNew.jsx
+++ src/component/todolist/TodoNew.jsx
@@ -4,13 +4,21 @@ const TodoNew =(props)=>{
   console.log('check props ',props);
   const name1 = "Hoàng Lâm";
   const {addNewTodo} = props ;
-  addNewTodo(name1);
+  // addNewTodo(name1);
+  // fire: khai hoả 
+  const handleClick = () => {
+    alert("Click me ")
+  }
+  const handleOnChange = (name) =>{
+    console.log(">> handleOnChange", name )
+  }
+
+
     return(
         <div>
-        <input type="text"/>
-        <button> Add </button>
+        <input type="text" onChange={(event)=>{handleOnChange(event.target.value)}}/>
+        <button style={{cursor: "pointer"}} onClick={handleClick}> Add </button>
       </div>
     );
 }
-
 export default TodoNew
