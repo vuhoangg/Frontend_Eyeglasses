296f979362c583442bebed722c2bb91f97164054
Bắc <you@example.com>
2025-01-14 21:31:16 +0700

    Tích hợp react_router_dom vào project

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,38 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/login",
+    element: <div> Login page </div>
+  },
+  {
+    path: "/register",
+    element: <div> Register page</div>
+  },
+  {
+    path: "/users",
+    element: <div> User page </div>
+  },
+  {
+    path: "/products",
+    element: <div> Product page </div>
+  }
+
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+        <RouterProvider router={router} />
+
   </StrictMode>,
 )
