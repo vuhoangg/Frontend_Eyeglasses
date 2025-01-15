4196cb71fb517b3de6b057860ad2785e3f94696c
Bắc <you@example.com>
2025-01-15 09:41:03 +0700

    #50 Use tab <NavLink/> raplace tab <Link/>

--- src/component/layout/header.jsx
+++ src/component/layout/header.jsx
@@ -1,16 +1,16 @@
 import './header.css'
-import { Link } from 'react-router-dom';
+import { Link, NavLink} from 'react-router-dom';
 const HeaderLayout= ()=>{
     // tab a = anchor khi ta sử dụng thể a thì ngày lập từ website bị load lại trang khi chuyển đường link 
     // vì vậy tả chuyển qua component <Link>
     return(
          <>
             <ul className="menu_bar">
-                <li><Link class="active" to="/">Home</Link></li>
-                <li><Link to="/user">User</Link></li>
-                <li><Link to="/product">Product</Link></li>
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="/register">Register</Link></li>
+                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to="/user">User</NavLink></li>
+                <li><NavLink to="/product">Product</NavLink></li>
+                <li><NavLink to="/login">Login</NavLink></li>
+                <li><NavLink to="/register">Register</NavLink></li>
             </ul>
         </>
     );
